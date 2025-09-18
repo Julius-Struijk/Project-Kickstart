ca43fb2e0881a6ecf068830b2afd360b39d33c8f
Julius Struijk <julius.struijk@gmail.com>
2025-09-18 17:32:26 +0200

    Created Player Setup Screen

---
+++ Assets/Scripts/SetupPlayers.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class SetupPlayers : MonoBehaviour
+{
+    Dictionary<string, GameObject> players;
+    List<string> playerNames;
+
+    //Save player info once setup is complete.
+    public void SavePlayerInfo()
+    {
+        //for(int i = 0; i < gameObject.transform.childCount; i++)
+        //{
+        //    Transform child = gameObject.transform.GetChild(i);
+        //    for (int j = 0; j < child.childCount; j++)
+        //    {
+        //        // Check which player object it is.
+        //        if(child.GetChild(i).gameObject.CompareTag("TextInput"))
+        //        GameObject  = child.GetChild(j).gameObject;
+
+
+        //    }
+
+        //}
+    }
+}
